26e9f0ce1d1d036cf467e6944a05a598abf16ea2
codemonkey721 <code.monkey721@gmail.com>
2023-04-21 05:16:29 -0700

    Update app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>I've been trying hard to deploy this site.</p>
+        <p>Finally, I've got this! Lol!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
